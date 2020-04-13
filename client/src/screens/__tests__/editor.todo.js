ad9f020d45444e0c6e53bee132199e9b7a2cc402
dzmitriy-melnikov <dimaira2012@yandex.ru>
2020-04-13 14:17:09 +0300

    unit tests react components 1 part finished

--- client/src/screens/__tests__/editor.todo.js
+++ client/src/screens/__tests__/editor.todo.js
@@ -1,19 +1,48 @@
-test('calls onSubmit with the username and password when submitted', () => {
-  // Arrange
-  // create a fake user, post, history, and api
-  //
-  // use ReactDOM.render() to render the editor to a div
-  //
-  // fill out form elements with your fake post
-  //
-  // Act
-  // submit form
-  //
-  // wait for promise to settle
-  //
-  // Assert
-  // ensure the create function was called with the right data
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Editor from '../editor.todo'
+import * as utilsMock from '../../utils/api'
+
+jest.mock('../../utils/api', () => {
+  return {
+    posts: {
+      create: jest.fn(() => Promise.resolve()),
+    },
+  }
 })
 
-// TODO later...
-test('snapshot', () => {})
+const flashPromises = () => {
+  return new Promise(resolve => {
+    setTimeout(resolve, 0)
+  })
+}
+
+test('calls onSubmit with the username and password when submitted', async () => {
+  const container = document.createElement('div')
+  const fakeUser = {id: 'fale'}
+  const fakeHistory = {
+    push: jest.fn(),
+  }
+
+  ReactDOM.render(<Editor user={fakeUser} history={fakeHistory} />, container)
+  const form = container.querySelector('form')
+  const {title, content, tags} = form.elements
+  title.value = 'I like snick'
+  content.value = 'I like cool'
+  tags.value = 'twix,          my     , likes'
+  const submit = new window.Event('submit')
+  form.dispatchEvent(submit)
+
+  await flashPromises()
+
+  expect(fakeHistory.push).toHaveBeenCalledTimes(1)
+  expect(fakeHistory.push).toHaveBeenCalledWith('/')
+  expect(utilsMock.posts.create).toHaveBeenCalledTimes(1)
+  expect(utilsMock.posts.create).toHaveBeenCalledWith({
+    authorId: fakeUser.id,
+    content: content.value,
+    tags: ['twix', 'my', 'likes'],
+    title: title.value,
+    date: expect.any(String),
+  })
+})
